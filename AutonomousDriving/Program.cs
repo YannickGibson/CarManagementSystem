f747d5c07730838f761fd6624cecb96b3c2f2939
Yannick Gibson <yannick.gibson@seznam.cz>
2019-12-09 21:33:26 +0100

    Converting to move per second, and making all central from Control Centre

--- AutonomousDriving/Program.cs
+++ AutonomousDriving/Program.cs
@@ -8,17 +8,16 @@ using System.Diagnostics;
 
 namespace AutonomousDriving
 {
-    enum Obstacle { Nothing, Bridge, Tunnel}
+    enum Track { Nothing, Bridge, Tunnel}
     enum Weather { Normal, Rainy, Icy}
     class Program
     {
         static Random rnd = new Random();
-        static void Main(string[] args)
+        static void Main(string[] args)//each char is 100m!!
         {
             MeteorologicCentrum mCentrum = new MeteorologicCentrum();
             DriveControl control = new DriveControl(numOfCars: 25);
             mCentrum.OnWeatherChanged += control.WeatherHasChanged;
-            control.UpdateRoad();
             while (true)
             {
                 foreach (var v in control.Run())
@@ -40,7 +39,9 @@ namespace AutonomousDriving
             {
                 for (int i = 0; i < numOfCars; i++)
                 {
-                    cars.Add(new Tesla(rnd.Next(1000,5000)));
+                    Tesla t = new Tesla(rnd.Next(1000, 5000));
+                    t.OnTrackChanged += this.TrackChanged;
+                    cars.Add(t);
                 }
             }
             public IEnumerable<int>Run()
@@ -50,21 +51,79 @@ namespace AutonomousDriving
                 {
                     foreach (var car in cars)
                     {
-                        car.StayOrMove(stopwatch.ElapsedMilliseconds, weather);
+                        if (car.Path.Length - 1 <= car.PositionIndex)
+                        {
+                            car.Completed = true;
+                        }
+                        else
+                        {
+                            float timeWithInterval = 0;
+
+                            float addingInterval = 0;
+
+                            if (weather == Weather.Normal)
+                                addingInterval = car.NormalInterval;
+                            else if (weather == Weather.Rainy)
+                                addingInterval = car.RainyInterval;
+                            else if (weather == Weather.Icy)
+                                addingInterval = car.IcyInterval;
+
+                            addingInterval /= car.TrackSpeedMultiplier;
+                            car.CurrentInterval = (int)addingInterval;
+
+                            timeWithInterval = car.LastMovedAt + addingInterval;
+
+
+
+
+                            if (stopwatch.ElapsedMilliseconds > timeWithInterval)
+                            {
+                                car.Move(stopwatch.ElapsedMilliseconds);
+                            }
+                        }
                     }
-                    
+
                     yield return 0;
+                }
+            }
+
+            public float GetSpeedOf(int ind)
+            {
+                float interval = cars[ind].CurrentInterval / cars[ind].TrackSpeedMultiplier;
+                double speedPerSecond = 1000 / interval;
+                double speedPerHour = speedPerSecond / 3600;
+                double speed = speedPerHour * 100_000;//speeding onesecond realtime 100k times
+                return (float)speedPerSecond;
+            }
+
+            public void TrackChanged(Tesla car)
+            {
+                if (car.CurrentTrack == Track.Nothing)
+                {
+                    car.LightsOn = false;
+                    car.TrackSpeedMultiplier = 1f;
+                }
+                else if (car.CurrentTrack == Track.Bridge)
+                {
+                    car.LightsOn = false;
+                    car.TrackSpeedMultiplier = 0.8f;
+                }
+                else if (car.CurrentTrack == Track.Tunnel)
+                {
+                    car.LightsOn = true;
+                    car.TrackSpeedMultiplier = 5f;
                 }
             }
             public void UpdateRoad()
             {
+                ConsoleColor currColor = new ConsoleColor();
                 if (weather == Weather.Normal)
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    currColor = ConsoleColor.Black;
                 else if (weather == Weather.Rainy)
-                    Console.BackgroundColor = ConsoleColor.DarkBlue;
-                else if (weather == Weather.Icy)
-                    Console.BackgroundColor = ConsoleColor.DarkGray;
-                
+                    currColor = ConsoleColor.DarkBlue;
+                else// if (weather == Weather.Icy)
+                    currColor = ConsoleColor.DarkGray;
+                Console.BackgroundColor = currColor;
                 string s = "";
                 for (int i = 0; i < cars.Count; i++)
                 {
@@ -72,15 +131,35 @@ namespace AutonomousDriving
                 }
                 Console.Clear();
                 string temp = "";//for performance reasons
-                foreach (var c in s)
+                int carIndex = 0;
+                int carIndexTemp = -1;
+                for (int i = 0; i < s.Length; i++)
                 {
+                    char c = s[i];
+                    if (carIndexTemp == -1 || carIndex != carIndexTemp && ( s[i - 1] == '\n' || s[i - 1] == '\r'))
+                    {
+                        carIndexTemp = carIndex;
+                        temp += $"{GetSpeedOf(carIndex): 0.0}/s ";
+                    }
                     if (c == 'X')
                     {
                         Console.Write(temp);
                         temp = "";
                         Console.ForegroundColor = ConsoleColor.Red;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
                         Console.Write(c);
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.BackgroundColor = currColor;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (i -1  > 0 && s[i - 1] == 'X')
+                    {
+                        if(cars[carIndex].LightsOn == true)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.Write(c);
+                            Console.BackgroundColor = currColor;
+                        }
+                        carIndex++;
                     }
                     else
                         temp += c;
@@ -95,79 +174,99 @@ namespace AutonomousDriving
         }
         class Tesla
         {
-            Obstacle[] path;
+            public Track[] Path { get; set; }
             public int PositionIndex { get; set; }
-            public bool Completed { get; private set; } = false;
-            public long TimeElapsed { get; private set; }
+            public bool Completed { get; set; } = false;
+            public long LastMovedAt { get; private set; }
 
-            public int Interval { get; set; }
-            private int RainyInterval = 4000;
-            private int IcyInterval = 6000;
+            public int CurrentInterval { get; set; }
+            public int NormalInterval { get; set; }
+            public int RainyInterval { get; set; } = 4000;
+            public int IcyInterval { get; set; } = 6000;
 
+            public Track CurrentTrack { get; set; }
+
+            public delegate void TrackChanged(Tesla t);
+
+            public event TrackChanged OnTrackChanged;
+            
+            public bool LightsOn { get; set; }
+            public float TrackSpeedMultiplier { get; set; } = 1;
             public Tesla(int interval, int positionIndex = 0)
             {
+                CurrentTrack = Track.Nothing;
                 this.PositionIndex = positionIndex;//
-                this.Interval = interval;
-                path = new Obstacle[rnd.Next(50, 70)];
-                for (int i = 0; i < path.Length; i++)
+                this.NormalInterval = interval;
+
+
+                Path = new Track[rnd.Next(50, 70)];
+                for (int i = 0; i < Path.Length; i++)
                 {
                     int nothingChance = rnd.Next(0, 10);
                     if(nothingChance == 0)
                     {
-                        path[i] = (Obstacle)rnd.Next(1, 3);
+                        Path[i] = (Track)rnd.Next(1, 3);
+                        if (i != 0)//makes the tunnel/bridge longer if can
+                        {
+                            while (rnd.Next(0, 5) != 0)//4 in 5 chance to continue 
+                            {
+                                if (i < Path.Length - 1)
+                                {
+                                    i++;
+                                    Path[i] = Path[i - 1];
+                                }
+                                else
+                                {
+                                    break;
+                                }
+
+                            }
+                        }
+                        
+
                     }
                     else
                     {
-                        path[i] = Obstacle.Nothing;
+                        Path[i] = Track.Nothing;
                     }
                 }
             }
-
-            public void StayOrMove(long elapsedMilliseconds,Weather weather)
+            public void Move(long elapsedMilliseconds)
             {
-                
-                if(path.Length -1 <= PositionIndex)
+                this.PositionIndex++;
+                LastMovedAt = elapsedMilliseconds;
+                if (OnTrackChanged != null)
                 {
-                    Completed = true;
-                    return;
+                    if (Path.Length > PositionIndex + 1 && Path[PositionIndex + 1] != CurrentTrack)
+                    {
+                        CurrentTrack = Path[PositionIndex + 1];
+                        OnTrackChanged.Invoke(this);
+                    }
                 }
-
-                long timeToReach = 0; 
-                if (weather == Weather.Normal)
-                    timeToReach = TimeElapsed + Interval;
-                else if (weather == Weather.Rainy)
-                    timeToReach = TimeElapsed + RainyInterval;
-                else if (weather == Weather.Icy)
-                    timeToReach = TimeElapsed + IcyInterval;
-
-                if (path[PositionIndex] == Obstacle.Bridge)
-                    timeToReach -= (timeToReach - TimeElapsed)/3;// speed -30%
-                else if (path[PositionIndex] == Obstacle.Tunnel)
-                    timeToReach += (timeToReach - TimeElapsed) / 2;// speed +50%
-
-                if (elapsedMilliseconds > timeToReach)
-                {
-                    this.PositionIndex++;
-                    TimeElapsed = elapsedMilliseconds;
-                }
-
-
             }
             public string GetPath()
             {
                 string s = "";
-                for (int i = 0; i < path.Length; i++)
+                for (int i = 0; i < Path.Length; i++)
                 {
                     if (i == PositionIndex)
                         s += "X";
-                    if (path[i] == Obstacle.Nothing)
+                    if (Path[i] == Track.Nothing)
                         s += "-";
-                    else if (path[i] == Obstacle.Bridge)
+                    else if (Path[i] == Track.Bridge)
                         s += "M";
-                    else if (path[i] == Obstacle.Tunnel)
-                        s += "U";
+                    else if (Path[i] == Track.Tunnel)
+                        s += "_";
                 }
                 return s + "\n";
+            }
+        }
+        class TrackChangedArgs : EventArgs
+        {
+            public Tesla tesla { get; set; }
+            public TrackChangedArgs(Tesla tesla)
+            {
+                this.tesla = tesla;
             }
         }
         class MeteorologicCentrum
